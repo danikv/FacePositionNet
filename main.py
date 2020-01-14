1b683ef6e8e5b9ca0873a34a2e99d8068a3b5d79
DanielKovachev <danielkv8@gmail.com>
2020-01-15 00:28:01 +0200

    rotations

--- main.py
+++ main.py
@@ -2,21 +2,40 @@ from collections import OrderedDict
 import numpy as np
 import cv2
 import argparse
-from utils.preprocess import preprocess_image
+from utils.preprocess import preprocess_images, calculate_landmarks, flip_landmarks, translate_image, rotate_and_scale_image, calculate_landmarks_with_respect_to_matrix
 from utils.model_loader import ModelLoader
  
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
 	help="path to input image")
+ap.add_argument("-o", "--output_csv", required=True,
+	help="path to output the csv")
 args = vars(ap.parse_args())
 
 model_loader = ModelLoader('model/model3D.mat', 'model/shape_predictor_68_face_landmarks.dat')
 
-image, landmarks, bbox = preprocess_image(args['image'], model_loader)
+#df = preprocess_images(args['image'], model_loader)
+#np.savetxt('struct_array.csv', df, delimiter=',', header='file name,rx,ry,ry,tx,ty,tz')
 
-for i, single_landmark in enumerate(landmarks):
-    (x,y,w,h) = bbox[i]
+#df.to_csv(args['output_csv'])
+
+img = cv2.imread(args['image'], cv2.IMREAD_COLOR)
+landmarks, bbox = calculate_landmarks(img, model_loader)
+img, traslation_matrix = translate_image(img, 10, 20)
+img, rotation_matrix = rotate_and_scale_image(img, 15, 0.9)
+
+#image = np.flip(image, 1)
+#landmarks = np.flip(landmarks, 1)
+image = cv2.flip(img, 1)
+
+for i, landmark in enumerate(landmarks):
+    landmark = calculate_landmarks_with_respect_to_matrix(landmark, traslation_matrix)
+    landmark = calculate_landmarks_with_respect_to_matrix(landmark, rotation_matrix)
+    landmark = flip_landmarks(image, landmark)
+
+    #for i, single_landmark in enumerate(landmarks):
+    (x,y,w,h) = bbox[0]
     cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     # show the face number
@@ -25,9 +44,10 @@ for i, single_landmark in enumerate(landmarks):
 
     # loop over the (x, y)-coordinates for the facial landmarks
     # and draw them on the image
-    for (x, y) in landmarks[i]:
-        cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
+    for (x, y) in landmark:
+        cv2.circle(image, (int(x), int(y)), 1, (0, 0, 255), -1)
  
 # show the output image with the face detections + facial landmarks
 cv2.imshow("Output", image)
 cv2.waitKey(0)
+
--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,8 +1,29 @@
 from utils.camera_calibration import estimate_camera, matrix2angle
+from math import cos, sin, radians
+import pandas as pd
 import numpy as np
+import glob
 import cv2
 import os
 
+def rotate_and_scale_image(image, angle, scale):
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    m = cv2.getRotationMatrix2D(center, angle, scale)
+    rotated = cv2.warpAffine(image, m, (w, h))
+    return rotated, m
+
+def translate_image(image, horizontal_translation, vertical_translation):
+    height, width, _ = image.shape
+
+    m = np.float32([[1, 0, horizontal_translation], [0, 1, vertical_translation]])
+
+    return cv2.warpAffine(image, m, (width, height)), m
+
+def calculate_landmarks_with_respect_to_matrix(landmarks, matrix):
+    new_landmarks = np.ones((landmarks.shape[0], 3))
+    new_landmarks[:, :-1] = landmarks
+    return np.transpose(np.dot(matrix, np.transpose(new_landmarks)))
 
 def _shape_to_np(shape):
     coords = np.zeros((68, 2), dtype=np.float32)
@@ -43,12 +64,63 @@ def calculate_landmarks(img, model_loader):
     landmarks = np.asarray(landmarks, dtype='float32')
     return landmarks, bboxes
 
+def convert_data_to_dict(tvec, pose_angle, image_path):
+    record = {}
+    record['file name'] = image_path
+    record['rx'] = pose_angle[0]
+    record['ry'] = pose_angle[1]
+    record['rz'] = pose_angle[2]
+    record['tx'] = tvec[0][0]
+    record['ty'] = tvec[1][0]
+    record['tz'] = tvec[2][0]
+    print(record)
+    return record
+
+def flip_image(image):
+    return np.flip(image, 1)
+
+def flip_landmarks(image, landmarks):
+    _, width = image.shape[:2]
+    new_landmarks = []
+    for x,y in landmarks:
+        new_landmarks.append((width - x, y))
+    return np.array(new_landmarks)
+
+def random_horizontal_translation(img):
+    _, width = img.shape[:2]
+    return width * np.random.uniform(-0.1, 0.1)
+
+def random_vertical_translation(img):
+    height, _ = img.shape[:2]
+    return height * np.random.uniform(-0.1, 0.1)
+
+def random_scale():
+    return np.random.uniform(0.75, 1.25)
+
+def random_rotation():
+    return 45 * np.random.normal(0, 1)
+
 def preprocess_image(image_path, model_loader):
     img = cv2.imread(image_path, cv2.IMREAD_COLOR)
     landmarks, bbox = calculate_landmarks(img, model_loader)
-    for i, single_landmark in enumerate(landmarks):
+    records = []
+    for i, single_landmark in enumerate(landmarks):        
+        image, traslation_matrix = translate_image(img, random_horizontal_translation(img), random_vertical_translation(img))
+        image, rotation_matrix = rotate_and_scale_image(img, random_rotation(), random_scale())
+        single_landmark = calculate_landmarks_with_respect_to_matrix(single_landmark, traslation_matrix)
+        single_landmark = calculate_landmarks_with_respect_to_matrix(single_landmark, rotation_matrix)
+        image = flip(image)
+        single_landmark = flip(landmarks)
         proj_matrix, camera_matrix, rmat, tvec = estimate_camera(model_loader.model3D, single_landmark)
         pose_angle = matrix2angle(rmat)
-        print(pose_angle)
-        print(tvec)
-    return img, landmarks, bbox
+        #create image rotations
+        records.append(convert_data_to_dict(tvec, pose_angle, image_path))
+    return records
+
+def preprocess_images(images_folder, model_loader):
+    df = []
+    for img in os.listdir(images_folder):
+        if img.endswith(".jpg") or img.endswith(".png"):
+            for record in preprocess_image(os.path.join(images_folder, img), model_loader):
+                df.append(record)
+    return pd.DataFrame(df)
